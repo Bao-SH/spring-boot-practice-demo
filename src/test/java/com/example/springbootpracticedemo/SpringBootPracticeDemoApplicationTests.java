a20cf8a8ef68719cc505af2ddce5874225ee4889
Shuanghui Bao <baoshrainbow@126.com>
2023-12-13 14:08:09 +0800

    init project

---
+++ src/test/java/com/example/springbootpracticedemo/SpringBootPracticeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootpracticedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPracticeDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
