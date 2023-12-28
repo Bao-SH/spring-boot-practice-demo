2585da7fd0afbe8f56d925f48fcd5d399c97cb33
Shuanghui Bao <baoshrainbow@126.com>
2023-12-28 15:32:57 +0800

    init integrate with logback module

---
+++ integrate-with-logback/src/main/java/com/example/integratewithdatadog/IntegrateWithDatadogApplication.java
@@ -0,0 +1,13 @@
+package com.example.integratewithdatadog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrateWithDatadogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegrateWithDatadogApplication.class, args);
+    }
+
+}
---
+++ integrate-with-logback/src/test/java/com/example/integratewithdatadog/IntegrateWithDatadogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.integratewithdatadog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrateWithDatadogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
