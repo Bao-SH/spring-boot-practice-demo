995025d0ab67512fb3034f25c5ba57cdc7ac48e1
Shuanghui Bao <baoshrainbow@126.com>
2024-01-03 14:17:25 +0800

    init logback consumer module

---
+++ logback-consumer/src/main/java/com/example/logbackconsumer/LogbackConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.logbackconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogbackConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogbackConsumerApplication.class, args);
+    }
+
+}
