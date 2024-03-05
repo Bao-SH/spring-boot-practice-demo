a19906d6e09ca983acb38c0459d732190ed0f6d3
Shuanghui Bao <baoshrainbow@126.com>
2024-03-05 15:34:43 +0800

    init solace module

---
+++ integrate-with-solace/src/main/java/com/example/integratewithsolace/IntegrateWithSolaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.integratewithsolace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrateWithSolaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegrateWithSolaceApplication.class, args);
+    }
+
+}
