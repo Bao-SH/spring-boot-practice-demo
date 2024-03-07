55afc04101a5932baba00553b1cc900696cd2dfc
Shuanghui Bao <baoshrainbow@126.com>
2024-03-07 15:24:51 +0800

    add processor

--- spring-cloud-stream-demo/src/main/java/com/example/springcloudstreamdemo/SpringCloudStreamDemoApplication.java
+++ spring-cloud-stream-demo/src/main/java/com/example/springcloudstreamdemo/SpringCloudStreamDemoApplication.java
@@ -1,13 +1,35 @@
 package com.example.springcloudstreamdemo;
 
+import com.example.springcloudstreamdemo.datamodel.SensorReading;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Function;
 
 @SpringBootApplication
 public class SpringCloudStreamDemoApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(SpringCloudStreamDemoApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudStreamDemoApplication.class, args);
     }
 
+    @Bean
+    public Function<SensorReading, SensorReading> convertFtoC() {
+        return reading -> {
+            log.info("Received: " + reading);
+
+            double temperatureCelsius = (reading.getTemperature() - 32) * 5 / 9;
+            reading.setTemperature(temperatureCelsius);
+            reading.setBaseUnit(SensorReading.BaseUnit.CELSIUS);
+
+            log.info("Sending: " + reading);
+
+            return reading;
+        };
+    }
 }
